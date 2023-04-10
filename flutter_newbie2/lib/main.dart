bcd3c752566ca99b1b3c03e5a5d8caca34c35c12
ybminm <superybm@outlook.kr>
2023-04-10 21:18:15 +0900

    Do exercise 4_responsable button

--- flutter_newbie2/lib/main.dart
+++ flutter_newbie2/lib/main.dart
@@ -1,36 +1,47 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () {
-          print("MyButton tapped!");
-        },
-        child: Container(
-          height: 50.0,
-          padding: const EdgeInsets.all(8.0),
-          margin: const EdgeInsets.symmetric(horizontal: 8.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5.0),
-            color: Colors.lightGreen,
-          ),
-          child: const Center(
-            child: Text("Engage"),
-          ),
-        ));
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _increment,
+          child: const Text('Increment'),
+        ),
+        const SizedBox(
+          width: 16.0,
+        ),
+        Text('Count: $_counter'),
+      ],
+    );
   }
 }
 
 void main() {
-  runApp(const MaterialApp(
-    title: "Detecting gesture",
-    home: Scaffold(
-      body: Center(
-        child: MyButton(),
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Counter(),
+        ),
       ),
     ),
-  ));
+  );
 }
